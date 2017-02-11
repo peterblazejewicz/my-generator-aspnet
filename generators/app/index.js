42ba0ef0dc011311c37b9d153a094dfed079f6c8
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-02-11 20:05:26 +0100

    Update syntax to ES6 (Node 4.*) and NPM updates
    - dependnecies bumped to latest versions
    - syntax rewritten to ES6
    - syntext corrected to layter Yo version

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,37 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
 
-module.exports = yeoman.Base.extend({
-  prompting: function () {
+module.exports = class extends Generator {
+  prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the polished ' + chalk.red('generator-aspnet') + ' generator!'
-    ));
+    this.log(yosay('Welcome to the polished ' + chalk.red('generator-aspnet') + ' generator!'));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to enable this option?',
+        default: true
+      }
+    ];
 
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
-  },
+    return this
+      .prompt(prompts)
+      .then(function (props) {
+        // To access props later use this.props.someAnswer;
+        this.props = props;
+      }.bind(this));
+  }
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  },
+  writing() {
+    this
+      .fs
+      .copy(this.templatePath('dummyfile.txt'), this.destinationPath('dummyfile.txt'));
+  }
 
-  install: function () {
+  install() {
     this.installDependencies();
   }
-});
+};
