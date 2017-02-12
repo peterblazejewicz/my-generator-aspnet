92952e68c425b40312310bcb987650c66cc53268
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-02-12 22:07:40 +0100

    Working draft of Yo generator using dotnet/cli
    Use dotnet new command with Yo spawn features

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,36 +2,55 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const helper = require('../../lib/utils/templates');
 
 module.exports = class extends Generator {
+
+  constructor(args, opts) {
+    super(args, opts);
+    // override default options like appName;
+    this.appName = this.appName;
+  }
+  initializing() {
+    this.templates = helper.getAllTemplates();
+  }
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the polished ' + chalk.red('generator-aspnet') + ' generator!'));
-
+    const msg = `Welcome to the new, polished ${chalk.red('generator-aspnet')} generator!`;
+    this.log(yosay(msg));
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: 'list',
+        name: 'tags',
+        message: 'What type of dotnet template do you want to instantiate?',
+        choices: helper.getProjectChoices(),
+        pageSize: 10
       }
     ];
-
     return this
       .prompt(prompts)
-      .then(function (props) {
-        // To access props later use this.props.someAnswer;
-        this.props = props;
-      }.bind(this));
+      .then(answers => {
+        this.options.type = answers.tags.type;
+        this.options.language = answers.tags.language;
+      });
   }
+
+  configuring() {
+    // process answers
+  }
+
+  default() {}
 
   writing() {
-    this
-      .fs
-      .copy(this.templatePath('dummyfile.txt'), this.destinationPath('dummyfile.txt'));
+    this.log('dotnet', 'new', this.options.type, '-lang', this.options.language);
+    this.spawnCommandSync('dotnet', ['new', this.options.type, '-lang', this.options.language]);
   }
 
+  conflicts() {}
+
   install() {
-    this.installDependencies();
+    // this.installDependencies();
   }
+
+  end() {}
 };
