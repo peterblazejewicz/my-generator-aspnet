ac2d8a5a38d4d104e2b01dc5b5043a8283d91829
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-02-13 23:13:48 +0100

    Update tests to cover project and item content
    Both type of template content are now testable

--- test/app.js
+++ test/app.js
@@ -15,7 +15,8 @@ describe('dotnet new console -lang F#', () => {
         tags: {
           type: 'console',
           language: 'F#'
-        }
+        },
+        template: 'project'
       })
       .toPromise();
   });
@@ -38,13 +39,37 @@ describe('dotnet new console -lang C#', () => {
         tags: {
           type: 'console',
           language: 'C#'
-        }
+        },
+        template: 'project'
       })
       .toPromise();
   });
 
   it('creates project files', () => {
     const files = ['Program.cs', `${base}.csproj`];
+    assert.file(files);
+  });
+});
+
+describe('dotnet new sln', () => {
+  let base = null;
+  before(() => {
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .inTmpDir(dir => {
+        base = path.basename(dir);
+      })
+      .withPrompts({
+        tags: {
+          type: 'sln'
+        },
+        template: 'item'
+      })
+      .toPromise();
+  });
+
+  it('creates solution file', () => {
+    const files = [`${base}.sln`];
     assert.file(files);
   });
 });
