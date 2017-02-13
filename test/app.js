bea9bfa0c55c7d87e095f4b5f473e103a226f90f
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-02-13 22:16:19 +0100

    Add sample test coverage for dotnet new command
    - C# project
    - F# project

--- test/app.js
+++ test/app.js
@@ -3,15 +3,48 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-describe('generator-aspnet:app', () => {
+describe('dotnet new console -lang F#', () => {
+  let base = null;
   before(() => {
     return helpers
       .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+      .inTmpDir(dir => {
+        base = path.basename(dir);
+      })
+      .withPrompts({
+        tags: {
+          type: 'console',
+          language: 'F#'
+        }
+      })
       .toPromise();
   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+  it('creates project files', () => {
+    const files = ['Program.fs', `${base}.fsproj`];
+    assert.file(files);
+  });
+});
+
+describe('dotnet new console -lang C#', () => {
+  let base = null;
+  before(() => {
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .inTmpDir(dir => {
+        base = path.basename(dir);
+      })
+      .withPrompts({
+        tags: {
+          type: 'console',
+          language: 'C#'
+        }
+      })
+      .toPromise();
+  });
+
+  it('creates project files', () => {
+    const files = ['Program.cs', `${base}.csproj`];
+    assert.file(files);
   });
 });
